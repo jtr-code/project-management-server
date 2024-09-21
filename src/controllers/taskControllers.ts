4f308046b3631d31b6679c6a8ceede9920706c74
jtr-code <jtrtricks@gmail.com>
2024-09-21 20:37:07 +0530

    add : task controllers

---
+++ src/controllers/taskControllers.ts
@@ -0,0 +1,89 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getTasks = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { projectId } = req.query;
+
+    const tasks = await prisma.task.findMany({
+      where: {
+        projectId: Number(projectId),
+      },
+      include: {
+        author: true,
+        assignee: true,
+        comments: true,
+        attachments: true,
+      },
+    });
+    res.status(200).json(tasks);
+  } catch (error: any) {
+    res.status(500).json({
+      message: `Error in retrieving task ${error.message}`,
+    });
+  }
+};
+
+export const createTask = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      title,
+      description,
+      status,
+      priority,
+      tags,
+      startDate,
+      dueDate,
+      points,
+      projectId,
+      authorUserId,
+      assignedUserId,
+    } = req.body;
+
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        tags,
+        startDate,
+        dueDate,
+        points,
+        projectId,
+        authorUserId,
+        assignedUserId,
+      },
+    });
+
+    res.status(201).json(newTask);
+  } catch (error: any) {
+    res.status(500).json({
+      message: `Error in creating a task ${error.message}`,
+    });
+  }
+};
+
+export const updateTaskStatus = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { taskId } = req.params;
+    const { status } = req.body;
+
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: Number(taskId),
+      },
+      data: {
+        status: status,
+      },
+    });
+
+    res.json(updatedTask);
+  } catch (error: any) {
+    res.status(500).json({
+      message: `Error in updating task ${error.message}`,
+    });
+  }
+};
